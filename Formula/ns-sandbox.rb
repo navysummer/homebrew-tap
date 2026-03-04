c457629a3093ee798e53de4130cea9663032693f
navysummer <navysummer@yeah.net>
2026-03-05 00:14:27 +0800

    fix

--- Formula/ns-sandbox.rb
+++ Formula/ns-sandbox.rb
@@ -6,11 +6,11 @@ class NsSandbox < Formula
 
   on_macos do
     on_intel do
-      url "https://github.com/navysummer/ns-sandbox/releases/download/v0.0.1/ns-sandbox-\#{version}-x86_64.tar.gz"
+      url "https://github.com/navysummer/ns-sandbox/releases/download/v0.0.1/ns-sandbox-#{version}-x86_64.tar.gz"
       sha256 "9c37892e94909f6bd39a79fd3bc4f785b31be9d1bc3691bc23ba06ab7a6beb96"
     end
     on_arm do
-      url "https://github.com/navysummer/ns-sandbox/releases/download/v0.0.1/ns-sandbox-\#{version}-arm64.tar.gz"
+      url "https://github.com/navysummer/ns-sandbox/releases/download/v0.0.1/ns-sandbox-#{version}-arm64.tar.gz"
       sha256 "5a31dafa76824e5725751793317e2b9a8643b2760eb656c8e6d9e1e5b8715222"
     end
   end
