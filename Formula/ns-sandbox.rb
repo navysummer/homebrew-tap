b3e6f7a1dc33ff7e931babdd694ff69e021f45f6
navysummer <navysummer@yeah.net>
2026-06-28 19:22:01 +0800

    fix

--- Formula/ns-sandbox.rb
+++ Formula/ns-sandbox.rb
@@ -1,16 +1,16 @@
 class NsSandbox < Formula
   desc "Cross-platform command-line ns-sandbox"
   homepage "https://github.com/navysummer/ns-sandbox"
-  version "0.0.1"
+  version "0.0.2"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/navysummer/ns-sandbox/releases/download/v0.0.1/ns-sandbox-#{version}-x86_64.tar.gz"
+      url "https://github.com/navysummer/ns-sandbox/releases/download/v0.0.2/ns-sandbox-#{version}-x86_64.tar.gz"
       sha256 "9c37892e94909f6bd39a79fd3bc4f785b31be9d1bc3691bc23ba06ab7a6beb96"
     end
     on_arm do
-      url "https://github.com/navysummer/ns-sandbox/releases/download/v0.0.1/ns-sandbox-#{version}-arm64.tar.gz"
+      url "https://github.com/navysummer/ns-sandbox/releases/download/v0.0.2/ns-sandbox-#{version}-arm64.tar.gz"
       sha256 "5a31dafa76824e5725751793317e2b9a8643b2760eb656c8e6d9e1e5b8715222"
     end
   end
